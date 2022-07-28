7ba394fa3347c7efbfd0efccc32d7fd054f8f79c
Carlo Cedric Lijauco <clijauco@deloitte.com>
2022-07-28 17:50:52 +0800

    updated

--- Sources/AnalyticsPackage/FirebaseAnalyticsService.swift
+++ Sources/AnalyticsPackage/FirebaseAnalyticsService.swift
@@ -5,19 +5,19 @@
 //  Created by clijauco on 7/28/22.
 //
 import FirebaseAnalytics
-final class FirebaseAnalyticsService: AnalyticsServicing {
+public final class FirebaseAnalyticsService: AnalyticsServicing {
     public init() {}
     
-    func addEvent(event: AnalyticsEvent) {
+    public func addEvent(event: AnalyticsEvent) {
         Analytics.logEvent(event.name, parameters:event.parameters)
     }
     
-    func addScreen(event: AnalyticsEvent) {
+    public func addScreen(event: AnalyticsEvent) {
         Analytics.logEvent(AnalyticsEventScreenView, parameters: [AnalyticsParameterScreenName: event.name,
                                                                   AnalyticsParameterScreenClass: "\(event.name)Class"])
     }
     
-    func addUserProperty(userProperty: AnalyticsUserProperty) {
+    public func addUserProperty(userProperty: AnalyticsUserProperty) {
         Analytics.setUserProperty(userProperty.value, forName: userProperty.name)
     }
 }
